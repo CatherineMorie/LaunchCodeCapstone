349e1977e490543e45808dd959e86d5fc7882771
Catherine Morie <catherinemiget@gmail.com>
2019-07-17 19:30:42 -0500

    first commit

---
+++ src/main/java/org/launchcode/LaunchCodeCapstone/LaunchCodeCapstoneApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.LaunchCodeCapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunchCodeCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaunchCodeCapstoneApplication.class, args);
+	}
+
+}
